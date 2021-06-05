5f870287033478a496e688a98d7a77c3bd6e5898
Juan Ramirez <ciscojuan@gmail.com>
2021-06-04 23:16:16 -0500

    hook tvShow works

--- zinema/src/containers/App.jsx
+++ zinema/src/containers/App.jsx
@@ -1,62 +1,51 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from '../components/Header';
 import Search from '../components/Search';
+import Layout from '../components/Layout';
 import Categories from '../components/Categories';
 import Carousel from '../components/Carousel';
 import CarouselItem from '../components/CarouselItem';
 import Footer from '../components/Footer';
-import '../assets/styles/App.css'
+import '../assets/styles/App.scss'
+import useTvShowsApi from '../hooks/useTvShowsApi';
 
-const App = () =>(
-    <div className="App">
-        <Header />
-        <Search />
+const API = 'http://localhost:3001/initalState';
+//      Hoock
+const App = () => {
+    const initialState = useTvShowsApi(API);
+    return initialState.length === 0 ? <h1>Loading...</h1> : (
+        <Layout>
+            <Header />
+            <Search />
+            
+            {initialState.mylist.length > 0 && (
+                <Categories title="Mi lista">
+                    <Carousel>
+                        {initialState.mylist.map(item =>
+                            <CarouselItem key={item.id} {...item} />
+                        )}
+                    </Carousel>
+                </Categories>
+            )}
 
-        <Categories title="Mis Favoritos">
-            <Carousel>
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-            </Carousel>
-        </Categories>
+            <Categories title="Tendencias">
+                <Carousel>
+                    {initialState.trends.map(item =>
+                        <CarouselItem key={item.id} {...item} />
+                    )}
+                </Carousel>
+            </Categories>
 
-        <Categories title="Disney">
-            <Carousel>
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-            </Carousel>
-        </Categories>
-
-        <Categories title="Marvel">
-            <Carousel>
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-            </Carousel>
-        </Categories>
-
-        <Categories title="Pixar">
-            <Carousel>
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-            </Carousel>
-        </Categories>
-        <Footer />
-    </div>
-)
+            <Categories title="Originales de Platfix">
+                <Carousel>
+                    {initialState.originals.map(item =>
+                        <CarouselItem key={item.id} {...item} />
+                    )}
+                </Carousel>
+            </Categories>
+            <Footer />
+        </Layout>
+    );
+}
 
 export default App;
